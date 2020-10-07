b3f065bc0a59e65f308b9a91f36e1130bea531bc
songlin-tsai <71375010+songlin-tsai@users.noreply.github.com>
2020-10-07 17:00:07 +0800

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -2,20 +2,22 @@
 using namespace std;
 int main(int argc, char* argv[])
 {
-	if (argc != 3)
-	{
-		cout << "錯誤信息:" << endl;
-		cout << "輸入兩個名子齁" << endl;
-	}
+	
 
 
 	for (int i = 1; i < argc; i = i + 2)
 	{
 		string kw = argv[i];
-		if (kw == "-?") {
+		if (kw == "-?") 
+		{
+			cout << "錯誤信息:" << endl; 
 			cout << "輸入兩個名子啦" << endl;
 		}
-
+		else if (argc != 3)
+		{
+			cout << "錯誤信息:" << endl;
+			cout << "輸入兩個名子齁" << endl;
+		}
 
 		else {
 			cout << "Hello, " << argv[1] << "." << endl;
